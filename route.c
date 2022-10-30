88b546636bdfb4be2fc854ad1648b4703b6fd1e4
Meghan Harris <71603666+harrmegh@users.noreply.github.com>
2022-10-30 19:43:03 -0400

    Prettified and working

--- route.c
+++ route.c
@@ -20,8 +20,18 @@
 #include <netinet/ip_icmp.h>
 #include <netdb.h>
 
+/*
+ * Authors: Justin Sorensen, Meghan Harris, and Professor Kalafut
+ * Program: Simulate a router receiving ARP and ICMP Echo
+ * Request and forwarding respective replies.
+ */
+
+// Function to calculate checksum for ICMP
 unsigned short checksum(void *b, int len);
 
+// Struct to act as the ICMP header with the
+// properties we want: type, code, checksum, id,
+// and sequence number
 struct icmp_header
 {
   uint8_t type;
@@ -31,10 +41,25 @@ struct icmp_header
   uint16_t seqnum;
 };
 
+// Main program to simulate Router behavior
 int main()
 {
+  // Array to hold multiple sockets
   int packet_socket[10];
+  // File descriptor object
   fd_set myfds;
+  // Struct to iterate through interfaces
+  struct ifaddrs *ifaddr, *tmp;
+  // Struct to hold IP address
+  struct ifreq ifr;
+  // Holds MAC address as string
+  char macp[INET6_ADDRSTRLEN];
+  // For counting place in buffer
+  int counter = 0;
+  // Buffer to hold MAC and associated IP
+  char buffer[1024];
+  // For adding packet_socket elements
+  int index = 0;
   // get list of interface addresses. This is a linked list. Next
   // pointer is in ifa_next, interface name is in ifa_name, address is
   // in ifa_addr. You will have multiple entries in the list with the
@@ -42,17 +67,11 @@ int main()
   // common since most interfaces will have a MAC, IPv4, and IPv6
   // address. You can use the names to match up which IPv4 address goes
   // with which MAC address.
-  struct ifaddrs *ifaddr, *tmp, *interfaceAddr;
-  struct ifreq ifr;
-  char macp[INET6_ADDRSTRLEN];
   if (getifaddrs(&ifaddr) == -1)
   {
     perror("getifaddrs");
     return 1;
   }
-  int counter = 0;
-  char buffer[1024];
-  int index = 0;
   // have the list, loop over the list
   for (tmp = ifaddr; tmp != NULL; tmp = tmp->ifa_next)
   {
@@ -65,43 +84,44 @@ int main()
     {
       printf("Interface: %s\n", tmp->ifa_name);
       // create a packet socket on interface r?-eth1
-      // if (!strncmp(&(tmp->ifa_name[3]), "eth1", 4))
-      // {
       printf("Creating Socket on interface %s\n", tmp->ifa_name);
-      //  create a packet socket
+      //  create a packet socket at index
       //  AF_PACKET makes it a packet socket
       //  SOCK_RAW makes it so we get the entire packet
       //  could also use SOCK_DGRAM to cut off link layer header
       //  ETH_P_ALL indicates we want all (upper layer) protocols
       //  we could specify just a specific one
       packet_socket[index] = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
-      struct sockaddr_ll *s = (struct sockaddr_ll *)(tmp->ifa_addr);
-      int i;
-      int len = 0;
-      for (i = 0; i < 6; i++)
-      {
-        len += sprintf(macp + len, "%02X%s", s->sll_addr[i], i < 5 ? ":" : "");
-      }
-      printf("Interface name : %s\n Interface Address : %s\n", (ifaddr)->ifa_name, macp);
-      printf("Mac and macp size: %s %ld\n", macp, sizeof(macp));
-      memcpy(&buffer[counter], macp, sizeof(macp));
-      printf("Buffer mac at %d: %s\n", counter, &buffer[counter]);
-      counter += 46;
-
-      char *ip;
-      ifr.ifr_ifru.ifru_addr.sa_family = AF_INET;
-      strncpy(ifr.ifr_name, tmp->ifa_name, IFNAMSIZ - 1);
-      ioctl(packet_socket[index], SIOCGIFADDR, &ifr);
-      ip = inet_ntoa(((struct sockaddr_in *)&ifr.ifr_ifru.ifru_addr)->sin_addr);
-      printf("Ip and ip size : %s %ld\n", ip, sizeof(ip));
-      memcpy(&buffer[counter], ip, sizeof(ip));
-      printf("Buffer ip at %d: %s\n", counter, &buffer[counter]);
-      counter += 8;
       if (packet_socket[index] < 0)
       {
         perror("socket");
         return 2;
       }
+      // Place to store MAC address
+      struct sockaddr_ll *s = (struct sockaddr_ll *)(tmp->ifa_addr);
+      int i;
+      int len = 0;
+      // Change MAC to a string
+      for (i = 0; i < 6; i++)
+      {
+        len += sprintf(macp + len, "%02X%s", s->sll_addr[i], i < 5 ? ":" : "");
+      }
+      printf("Interface name : %s\n Interface Address : %s\n", (ifaddr)->ifa_name, macp);
+      printf("Mac: %s\n", macp);
+      // Copy MAC to buffer and increase counter
+      memcpy(&buffer[counter], macp, sizeof(macp));
+      counter += 46;
+
+      // Copy IP to buffer and increase counter
+      char *ip;
+      ifr.ifr_ifru.ifru_addr.sa_family = AF_INET;
+      strncpy(ifr.ifr_name, tmp->ifa_name, IFNAMSIZ - 1);
+      ioctl(packet_socket[index], SIOCGIFADDR, &ifr);
+      ip = inet_ntoa(((struct sockaddr_in *)&ifr.ifr_ifru.ifru_addr)->sin_addr);
+      printf("Ip: %s\n", ip);
+      memcpy(&buffer[counter], ip, sizeof(ip));
+      counter += 8;
+
       // Bind the socket to the address, so we only get packets
       // recieved on this specific interface. For packet sockets, the
       // address structure is a struct sockaddr_ll (see the man page
@@ -111,14 +131,13 @@ int main()
       if (bind(packet_socket[index], tmp->ifa_addr, sizeof(struct sockaddr_ll)) == -1)
       {
         perror("bind");
+        exit(8);
       }
-      else
-      {
-        FD_SET(packet_socket[index], &myfds);
-        FD_SET(STDIN_FILENO, &myfds);
-      }
+      // Set socket in file descriptor
+      FD_SET(packet_socket[index], &myfds);
+      FD_SET(STDIN_FILENO, &myfds);
+      // Prepare for the next socket
       index++;
-      // }
     }
   }
 
@@ -129,15 +148,19 @@ int main()
   printf("Ready to recieve now\n");
   while (1)
   {
-
+    // Place to hold packet we receive
     char buf[1500];
+    // Temp file descriptor
     fd_set fdstmp = myfds;
+    // Wait for an actual message
     int nn = select(FD_SETSIZE, &fdstmp, NULL, NULL, NULL);
+    // Struct to receive message
     struct sockaddr_ll recvaddr;
+    // Length of received message
     unsigned int recvaddrlen = sizeof(struct sockaddr_ll);
     for (int j = 0; j < 10; ++j)
     {
-
+      // If there is a packet in this socket
       if (FD_ISSET(packet_socket[j], &fdstmp))
       {
         // we can use recv, since the addresses are in the packet, but we
@@ -146,10 +169,8 @@ int main()
         // see packets in both directions. Only outgoing can be seen when
         // using a packet socket with some specific protocol)
         int n = recvfrom(packet_socket[j], buf, 1500, 0, (struct sockaddr *)&recvaddr, &recvaddrlen);
-        printf("Packet socket after receive: %d\n\n", packet_socket[j]);
-        printf("Got a %d byte packet\n", n);
+        // Temporary buffer to hold packet info
         char temp_buf[n];
-        printf("Size of temp buf now: %ld\n", sizeof(temp_buf));
 
         // ignore outgoing packets (we can't disable some from being sent
         // by the OS automatically, for example ICMP port unreachable
@@ -161,6 +182,7 @@ int main()
         eh.ether_type = ntohs(0x0000);
         memcpy(&eh, buf, 14);
 
+        // When the packet is an ICMP Echo Request
         if (ntohs(eh.ether_type) == 0x0800)
         {
           printf("ICMP\n\n");
@@ -170,79 +192,67 @@ int main()
 
           // build IP portion
           memcpy(&iph, &buf[14], sizeof(iph));
-
           memcpy(&iphResponse, &iph, sizeof(iph));
-
           memcpy(&iphResponse.saddr, &iph.daddr, sizeof(iph.daddr));
           memcpy(&iphResponse.daddr, &iph.saddr, sizeof(iph.saddr));
           // build EH portion
           memcpy(ehResponse.ether_dhost, eh.ether_shost, sizeof(eh.ether_shost));
           memcpy(ehResponse.ether_shost, eh.ether_dhost, sizeof(eh.ether_dhost));
           memcpy(&ehResponse.ether_type, &eh.ether_type, sizeof(eh.ether_type));
-
+          // Store all of the ICMP info
           memcpy(&icmp, &buf[34], sizeof(icmp));
-          printf("Temp buf size: %ld\n", sizeof(temp_buf));
-          // Verify checksum
+
           // Sequence num is the ttl -- 32 hops and done.  Decrement/Increment ?? if we hit 0, drop packet, and send ICMP time exceeded message.
-          printf("ICMP Struct type: %hhu, code: %hhu, checksum: %hhu, id: %hhu, sequence number: %hhu \n", icmp.type, icmp.code, icmp.checksum, icmp.id, icmp.seqnum);
+          // Set type and checksum to zero
           icmp.type = ntohs(0x0000);
           icmp.checksum = 0;
 
-          printf("ICMP Struct type: %hhu, code: %hhu, checksum: %hhu, id: %hhu, sequence number: %hhu \n", icmp.type, icmp.code, icmp.checksum, icmp.id, icmp.seqnum);
-
+          // Add everything to the message to send
           memcpy(&temp_buf, &ehResponse, sizeof(ehResponse));
           memcpy(&temp_buf[14], &iphResponse, sizeof(iphResponse));
           memcpy(&temp_buf[34], &icmp, sizeof(icmp));
           // Data - size of data is hard coded, so def need to change.
           memcpy(&temp_buf[42], &buf[42], n - 42);
-
+          // Calculate checksum
           icmp.checksum = checksum(&temp_buf[14], n - 14);
-
           memcpy(&temp_buf[36], &icmp.checksum, 2);
+          // Send ICMP Echo Reply
           int success = send(packet_socket[j], temp_buf, n, 0);
           if (success == -1)
           {
-            perror("sendto():");
+            perror("send():");
             exit(90);
           }
         }
         // when an ARP request is processed, respond
         else if (ntohs(eh.ether_type) == 0x0806)
         {
+          // Place to store MAC address
           char temp_mac[INET6_ADDRSTRLEN];
-
+          // Print statements to verify what we are receiving
           printf("Packet socket in ARP Request: %d\n\n", packet_socket[j]);
           printf("Received Ether Destination: %s\n", ether_ntoa((struct ether_addr *)&eh.ether_dhost));
           printf("Received Ether Source: %s\n", ether_ntoa((struct ether_addr *)&eh.ether_shost));
           printf("Received Ether Type: %s\n", ether_ntoa((struct ether_addr *)&eh.ether_type));
-
+          // Structs to hold our ARP ether information
           struct ether_arp arpReceived, arpResponse;
           memcpy(&arpReceived, &buf[14], sizeof(arpReceived));
 
+          // Find the right MAC address associated with IP
           for (int i = 0; i < sizeof(buffer); i += 54)
           {
-            // printf("Got into for loop\n");
-            // printf("Buffer at %d: %s\n", i, &buffer[i]);
-            // printf("IP at %d: %s\n", i + 46, &buffer[i + 46]);
-            // struct sockaddr_in sa;
             char temp_ip[9];
             char temp_tip[INET_ADDRSTRLEN];
             strncpy(temp_ip, &buffer[i + 46], 8);
-            // memcpy(&temp_ip, &buffer[i + 46], 8);
-            printf("IP %s\n", temp_ip);
             inet_ntop(AF_INET, &(arpReceived.arp_tpa), temp_tip, INET_ADDRSTRLEN);
-            // temp_tip = inet_ntoa((struct in_addr) & arpReceived.arp_tpa);
-            // memcpy(temp_arp.arp_tpa, &buffer[i + 46], 8);
-            printf("TIP %s\n", temp_tip);
             if (!strcmp(temp_ip, temp_tip))
             {
-              printf("Got here\n");
               memcpy(&temp_mac, &buffer[i], 46);
               break;
             }
           }
           printf("Mac in ARP: %s\n", temp_mac);
-
+          // Set ARP response to received and change op code to 2
           arpResponse = arpReceived;
           arpResponse.ea_hdr.ar_op = htons(2);
 
@@ -252,27 +262,26 @@ int main()
           memcpy(arpResponse.arp_spa, arpReceived.arp_tpa, sizeof(arpReceived.arp_tpa));
           memcpy(arpResponse.arp_sha, ether_aton(temp_mac), 6);
 
+          // Set up ether header
           struct ether_header ehResponse;
-
           memcpy(ehResponse.ether_dhost, eh.ether_shost, sizeof(eh.ether_shost));
           memcpy(ehResponse.ether_shost, arpResponse.arp_sha, 6);
           memcpy(&ehResponse.ether_type, &eh.ether_type, sizeof(eh.ether_type));
-
-          printf("Size of eh Response: %ld\n\n", sizeof(ehResponse));
+          // Store everything in the buffer
           memcpy(&temp_buf[0], &ehResponse, sizeof(ehResponse));
           memcpy(&temp_buf[14], &arpResponse, sizeof(arpResponse));
-
+          // Print statements to verify ether header info
           printf("Response Ether Destination: %s\n", ether_ntoa((struct ether_addr *)&ehResponse.ether_dhost));
           printf("Response Ether Source: %s\n", ether_ntoa((struct ether_addr *)&ehResponse.ether_shost));
           printf("Response Ether Type: %s\n\n", ether_ntoa((struct ether_addr *)&ehResponse.ether_type));
-
+          // Send ARP Reply
           int success = send(packet_socket[j], temp_buf, n, 0);
           if (success == -1)
           {
             perror("send():");
             exit(90);
           }
-
+          // Reset all of the info for type
           uint16_t reset = 0x0;
           memcpy(&eh.ether_type, &reset, sizeof(eh.ether_type));
         }
@@ -293,7 +302,6 @@ int main()
   // free the interface list when we don't need it anymore
   freeifaddrs(ifaddr);
   freeifaddrs(tmp);
-  freeifaddrs(interfaceAddr);
   for (int j = 0; j < 10; ++j)
     close(packet_socket[j]);
 
